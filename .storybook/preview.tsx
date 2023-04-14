eb2ce36a3e188274bc89a4e83f089c5041f356cc
Rob Ellison <rob.ellison@r&r.org.uk>
2023-04-14 11:44:28 +0000

    working no theme

---
+++ .storybook/preview.tsx
@@ -0,0 +1,27 @@
+import type { Preview } from "@storybook/react";
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { theme } from '../src/theme';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default preview;
