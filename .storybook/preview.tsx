e90e8dc1bc1e9fb2f19dd0b5e6894a2f07a28a02
Rob Ellison <rob.ellison@r&r.org.uk>
2023-04-15 22:49:25 +0000

    working banner

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,8 +2,17 @@ import type { Preview } from "@storybook/react";
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from '../src/theme';
+import React, { useEffect, useState } from 'react';
+import { StoryObj, Args } from '@storybook/react';
+import MDXProvider, { mdComponents } from '../src/stories/MDXProvider';
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote } from 'next-mdx-remote';
+import remarkGfm from "remark-gfm";
+import remarkUnwrapImages from 'remark-unwrap-images';
 
 const preview: Preview = {
+  
+    
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
@@ -12,16 +21,84 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    docs: {
+      source: {
+        // code: `<Banner>{children}</Banner>`,
+        // code: 'not supported',
+        // language: 'md',
+        type: 'code',
+        transform: (code,args) => {
+          console.log('transformSource:args: ', args)
+          return (
+            
+            '<' + args.component.name + getAttributes(args.args) +'/>\n'+
+            args.args.children
+            + '\n</' + args.component.name + '>'
+            
+            ) 
+        }
+      }
+    }
   },
+  
   decorators: [
-    (Story) => (
-      <ThemeProvider theme={theme}>
-      <CssBaseline />
-        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
-        <Story />
-      </ThemeProvider>
-    ),
+    (Story: StoryObj, context) => {
+      const [mdxContent, setMdxContent] = useState(null);
+
+      console.log('context:', context)
+      const { args } = context;
+      const { children } = args;
+
+      const mdx = `
+        <Banner>
+          ${children}
+        </Banner>
+      `;
+
+      useEffect(() => {
+        const serializeMdx = async () => {
+          const MDXoptions = {
+            remarkPlugins: [remarkGfm, remarkUnwrapImages],
+            format: 'mdx',
+            development: true
+          };
+          try {
+            const mdxSource = await serialize(mdx, { scope: {}, mdxOptions: { ...MDXoptions }, parseFrontmatter: true });
+            setMdxContent(mdxSource);
+
+          } catch (error) {
+            console.log('Error in serialize : ', error);
+          }
+        };
+        serializeMdx();
+      }, [mdx]);
+
+      if (!mdxContent) {
+        return <MDXProvider><h1>....loading</h1></MDXProvider>;
+      } else {
+
+        // return <MDXProvider ><HeaderCard {...args}><MDXRemote {...mdxContent} components={mdComponents} /></HeaderCard></MDXProvider>;
+        return  <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <MDXProvider>
+            <MDXRemote compiledSource={mdxContent.compiledSource} components={mdComponents} />
+          </MDXProvider>
+        </ThemeProvider>;
+      }
+    },
   ],
 };
+
+
+function getAttributes(args: Args): string {
+  let result = '';
+  for (const [key, value] of Object.entries(args)) {
+    if (value !== undefined && value !== null && key !== 'children') {
+      result += ` ${key}="${value}"`;
+    }
+  }
+  console.log('getAttributes :', result)
+  return result;
+}
 
 export default preview;
