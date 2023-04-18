e609a60291ea2670737382530b63cb046beb80a3
Rob Ellison <rob.ellison@r&r.org.uk>
2023-04-18 08:43:46 +0000

    remove eerything

--- src/MyComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MyComponent = ({ children }) => {
-  return <div>{children}</div>;
-};
-
-export default MyComponent;
--- src/index.js
+++
@@ -1,2 +0,0 @@
-export { default as MyComponent } from './MyComponent';
-// export other components here
